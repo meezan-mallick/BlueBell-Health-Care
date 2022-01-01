20781b0ae4e4d6aaa47838d5d72c7ae0172d6422
meezan-mallick <meezan.mallick@gmail.com>
2022-01-01 13:56:38 +0530

    config

---
+++ config/keys.js
@@ -0,0 +1,11 @@
+export default {
+    firebaseConfig: {
+      apiKey: "AIzaSyBT-P8M9XtwbpEluwiKuIETVgG9WkhCtmA",
+      authDomain: "blue-bell-2aec4.firebaseapp.com",
+      databaseURL: 'https://blue-bell-2aec4.firebaseio.com',
+      projectId: "blue-bell-2aec4",
+      storageBucket: "blue-bell-2aec4.appspot.com",
+      messagingSenderId: "715254431168",
+      appId: "1:715254431168:web:71325dddf2fddcf9ccdc25"
+    }
+  }
